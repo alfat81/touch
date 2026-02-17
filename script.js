67e088326be6ad4f33f209ceca2a216acdfeaa1c
Alexey F <52703823+alfat81@users.noreply.github.com>
2026-02-17 19:04:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Анимация появления при скролле
     const reveals = document.querySelectorAll('.reveal');
 
     const revealOnScroll = new IntersectionObserver((entries) => {
